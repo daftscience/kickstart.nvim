482ec16da91d9bcb5e6e0cea38c613c4e9437fbd
daftscience <tom@daftscience.com>
2024-05-18 12:22:05 -0400

    Starting my customizations

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+--
+--
+return {
+  { 'akinsho/toggleterm.nvim', version = '*', config = true },
+  {
+    'jim-at-jibba/micropython.nvim',
+    dependencies = { 'akinsho/toggleterm.nvim', 'stevearc/dressing.nvim' },
+  },
+}
